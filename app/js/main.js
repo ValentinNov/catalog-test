175c440962411cc70ca9cf14ad8fb2d9ba4c544d
Valentin nov <valentinnovosad83web@gmail.com>
2022-10-09 21:00:23 +0300

    two commit

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+
+$(function () {
+
+    $('.catalog__filter-btn').on('click', function () {
+        $('.col-filters').slideToggle();
+    })
+
+
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+        onChange: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+    });
+
+
+
+});
+
+
